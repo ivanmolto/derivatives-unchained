74b2d4172341514a63a371b7a1fca1d2403fe4c6
Ivan Molto <ivanmolto@gmail.com>
2021-05-05 00:33:34 +0200

    chore: Set up Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
